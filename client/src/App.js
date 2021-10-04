dca874c222cb0225878ded5350f20f794d3f35a6
Moreau <quentin.moreau@teads.tv>
2021-10-04 14:57:00 +0200

    creat react app

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import styled from 'styled-components'
+const MainWrapper = styled.div`
+  text-align: center ;
+  margin: 25px 50px;
+  background-color: white;
+  padding: 25px;
+  border-radius: 10px;
+`
+
+const App = () => {
+  return (
+    <MainWrapper>
+      <h3>Hello World</h3>
+    </MainWrapper>
+  );
+}
+
+export default App;
