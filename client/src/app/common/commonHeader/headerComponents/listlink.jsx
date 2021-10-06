7bf13a234c8491e1577ca926cb48eb2e3f04b56b
fatih_gucuko <web.contact@shadow-imperium.com>
2021-10-06 21:46:27 +0200

    mise a jours de generateur de menu

--- client/src/app/common/commonHeader/headerComponents/listlink.jsx
+++ client/src/app/common/commonHeader/headerComponents/listlink.jsx
@@ -1,55 +1,74 @@
-import styled from "styled-components"
-import { Link ,withRouter } from 'react-router-dom'
-  
+import styled from "styled-components";
+import { Link, withRouter } from "react-router-dom";
+
 const ListStyle = styled.ul`
-    display: flex;
-    flex-direction: row;
-    font-size: 20px;
-`
+  display: flex;
+  flex-direction: row;
+  font-size: 20px;
+`;
 const LiStyle = styled.li`
-    list-style: none;
-    margin-right: 80px;
-    margin-top: 14px;
-    cursor: pointer;
-   
-`
+  list-style: none;
+  margin-right: 80px;
+  margin-top: 14px;
+  cursor: pointer;
+`;
 
 const LinkStyle = styled(Link)`
-    color: gray;
-    text-decoration: none;
-    text-transform: uppercase;
-    &:hover {
-        color: black;
-    }
-`
+  color: gray;
+  text-decoration: none;
+  text-transform: uppercase;
+  &:hover {
+    color: black;
+  }
+`;
 
 const WrapperList = styled.div`
-    width: 20%;
-`
+  width: 20%;
+`;
 const LinkList = () => {
+  let List = ["accueil", "découvrir+", "règles", "Les troubles", "tarifs"];
 
-    const List = [
-        'accueil',
-        'découvrir+',
-        'règles',
-        'tarifs'
-    ]
+  const urls = [
+    new MenuLink({
+      name: "accueil",
+      uri: "/",
+    }),
+    new MenuLink({
+      name: "découvrir+",
+      uri: "/decouvrir",
+    }),
+    new MenuLink({
+      name: "règles",
+      uri: "/regles",
+    }),
+    new MenuLink({
+      name: "Les troubles",
+      uri: "/les-troubles-de-dys",
+    }),
+    new MenuLink({
+      name: "tarifs",
+      uri: "/tarifs",
+    }),
+  ];
 
-    return(
+  return (
+    <WrapperList>
+      <ListStyle>
+        {urls.map((menuLink) => (
+          <LiStyle key={menuLink.name}>
+            <LinkStyle to={menuLink.uri}>{menuLink.name}</LinkStyle>
+          </LiStyle>
+        ))}
+      </ListStyle>
+    </WrapperList>
+  );
+};
 
-        <WrapperList>
-            <ListStyle>
-                {   List.map
-                    (link => 
-                        <LiStyle key={link}>
-                            <LinkStyle to={`/${link}`} >{link}</LinkStyle>
-                        </LiStyle>
-                    )
-                }
-            </ListStyle>
-        </WrapperList>
-
-    )
+class MenuLink {
+  constructor({ name, uri }) {
+    this.name = name;
+    this.uri = uri;
+  }
 }
 
-export default withRouter(LinkList)
+export default withRouter(LinkList);
