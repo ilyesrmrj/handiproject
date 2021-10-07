bf4864947bcdb667897f0960d45d0ef6549619d0
fatih_gucuko <web.contact@shadow-imperium.com>
2021-10-07 14:58:44 +0200

    commenting mysql connection by default

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const connection = require("./config/db");
+// const connection = require("./config/db");
 
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/", (req, res) => {
