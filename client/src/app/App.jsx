b7016f4bb3da88ec80d8f53c6fe895129914e302
fatih_gucuko <web.contact@shadow-imperium.com>
2021-10-07 11:33:56 +0200

    ajout de la nouvelle route

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -7,6 +7,7 @@ import RulesPage from "./pages/rules/rules";
 import AppRoute from "./Route";
 import Trouble from "./pages/troubles/trouble";
 import PricePage from "./pricePage/pricePage";
+import Register from "./pages/registerSchool/register";
 
 const routes = [
   new AppRoute({
@@ -33,6 +34,11 @@ const routes = [
     name: "price",
     path: "/price",
     reactComponent: <PricePage />,
+  }),
+  new AppRoute({
+    name: "Register school",
+    path: "/register-school",
+    reactComponent: <Register />,
   }),
 ]
 
